e1ee6f7b9ff761cf894a97b2953295304fa926eb
Dmitrii Kim <mitya_kim@icloud.com>
2023-03-30 19:10:13 -0700

    feat: Add new project

---
+++ SwiftUI_And_CoreData/SwiftUI_And_CoreData/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_And_CoreData
+//
+//  Created by Mitya Kim on 3/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_And_CoreData/SwiftUI_And_CoreData/SwiftUI_And_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_And_CoreDataApp.swift
+//  SwiftUI_And_CoreData
+//
+//  Created by Mitya Kim on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_And_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
